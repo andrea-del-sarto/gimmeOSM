659d426eb65c20a2bf1ca0eed8cb60f68dfe6107
andrea-del-sarto <37538844+andrea-del-sarto@users.noreply.github.com>
2022-12-15 22:09:48 +0100

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,3 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmVhMGJhcmJhIiwiYSI6ImNsYnBqdTR1cjAyNGkzcnE3bDNyM29yMnMifQ.YgRRggrXkzyZWs9ILMpkwg';
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
-
-map.on('load', () => {
-    map.addLayer({
-        id: 'terrain-data',
-        type: 'line',
-        source: {
-            type: 'vector',
-            url: 'mapbox://mapbox.mapbox-terrain-v2'
-        },
-        'source-layer': 'contour'
-    });
-});
-
 var osmGeoJSON;
 
 function getOSM(type, id) {
