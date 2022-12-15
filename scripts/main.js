ec731103d3cef5e6428b8b6d4ac6f72895f3db7b
andrea-del-sarto <37538844+andrea-del-sarto@users.noreply.github.com>
2022-12-15 22:05:15 +0100

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,17 @@ const map = new mapboxgl.Map({
     zoom: 9 // starting zoom
 });
 
-
-var attribution = L.control.attribution();
-attribution.setPrefix('');
-attribution.addAttribution('<a href="https://github.com/andrea-del-sarto/gimmeOSM">gimme OSM Github</a> | <a href="https://www.mapbox.com/about/maps">© Mapbox</a> <a href="http://openstreetmap.org/copyright">© OpenStreetMap</a> | <a href="http://mapbox.com/map-feedback/" class="mapbox-improve-map">Improve this map</a>');
-attribution.addTo(map);
+map.on('load', () => {
+    map.addLayer({
+        id: 'terrain-data',
+        type: 'line',
+        source: {
+            type: 'vector',
+            url: 'mapbox://mapbox.mapbox-terrain-v2'
+        },
+        'source-layer': 'contour'
+    });
+});
 
 var osmGeoJSON;
 
